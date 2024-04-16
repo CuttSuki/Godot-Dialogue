f06ac61a488452409b524ddd3888df59e5c9d983
CuttSuki <167149506+CuttSuki@users.noreply.github.com>
2024-04-16 08:11:38 +0700

    Update NPC.gd

--- Dialogue Test/NPC.gd
+++ Dialogue Test/NPC.gd
@@ -14,6 +14,8 @@ func showDialogue():
 		dialogue.get_node("Label").text = dialogueDict[dialogueIndex]
 		if Input.is_action_just_pressed("ui_accept") and dialogueIndex < dialogueDict.size() - 1:
 			dialogueIndex +=1
+		elif dialogueIndex == dialogueDict.size() - 1:
+			canInteract = false
 func _process(delta):
 	if not canInteract:
 		dialogue.visible = false
